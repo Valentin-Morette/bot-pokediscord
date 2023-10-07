0b253db6bc77848c7f1eeb93c96e1401f88a5a0b
valentin-morette <morette.valentin@gmail.com>
2023-10-07 11:07:13 +0200

    prevent crash

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ import pokeChat from './pokechat.js';
 dotenv.config();
 
 pokeChat(clientPokechat);
+
+process.on('uncaughtException', (error) => {
+	console.error('Uncaught Exception:', error);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+	console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
